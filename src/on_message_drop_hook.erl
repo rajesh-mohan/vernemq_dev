421ea30195cad2fb3b7b0c99c39e3d8cac5b54a0
Rajesh Mohan <50743110+rajesh-mohan@users.noreply.github.com>
2023-10-31 22:20:50 +0530

    Update on_message_drop_hook.erl

--- src/on_message_drop_hook.erl
+++ src/on_message_drop_hook.erl
@@ -4,7 +4,7 @@
 
 %% called as an 'all'-hook, return value is ignored
 -callback on_message_drop(SubscriberId  :: subscriber_id(),
-                          Promise       :: fun(() -> {topic(), qos(), payload(), map()} | error),
+                          Promise       :: fun(() -> {topic(), qos(), payload()} | error),
                           Reason        :: expired
                                          | queue_full
                                          | max_packet_size_exceeded) -> any().
